d76dbeb4bf110271d8e08f1681a689122271418a
wesleyEmidio <wesley_esc1987@hotmail.com>
2023-01-18 14:30:35 -0300

    Exceção ao atualizar usuario

--- src/main/java/com/estudosweb/curso/servicos/UsuarioServico.java
+++ src/main/java/com/estudosweb/curso/servicos/UsuarioServico.java
@@ -13,41 +13,48 @@ import com.estudosweb.curso.entidades.Usuario;
 import com.estudosweb.curso.servicos.excecoes.BaseDadosExcecao;
 import com.estudosweb.curso.servicos.excecoes.RecursoNaoEncontradoExcecao;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UsuarioServico {
 
 	@Autowired
 	private UsuarioRepositorio repositorio;
-	
-	public List<Usuario> retornaUsuarios(){
+
+	public List<Usuario> retornaUsuarios() {
 		return repositorio.findAll();
 	}
-	
+
 	public Usuario retornaUsuarioPorId(Long id) {
 		Optional<Usuario> usuario = repositorio.findById(id);
 		return usuario.orElseThrow(() -> new RecursoNaoEncontradoExcecao(id));
 	}
-	
+
 	public Usuario inserir(Usuario usuario) {
 		return repositorio.save(usuario);
 	}
-	
+
 	public void excluir(Long id) {
 		try {
 			repositorio.deleteById(id);
-			
+
 		} catch (EmptyResultDataAccessException e) {
 			throw new RecursoNaoEncontradoExcecao(id);
-			
+
 		} catch (DataIntegrityViolationException e) {
 			throw new BaseDadosExcecao(e.getMessage());
 		}
 	}
-	
+
 	public Usuario atualizar(Long id, Usuario usuarioNovo) {
-		Usuario usuarioAtual = repositorio.getReferenceById(id);
-		atualizarRegistro(usuarioAtual, usuarioNovo);
-		return repositorio.save(usuarioAtual);
+		try {
+			Usuario usuarioAtual = repositorio.getReferenceById(id);
+			atualizarRegistro(usuarioAtual, usuarioNovo);
+			return repositorio.save(usuarioAtual);
+			
+		} catch (EntityNotFoundException e) {
+			throw new RecursoNaoEncontradoExcecao(id);
+		}
 	}
 
 	private void atualizarRegistro(Usuario usuarioAtual, Usuario usuarioNovo) {
